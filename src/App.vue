0b041083c3f9e85d53781f2a223492fba2ee3ded
Faisal Akhtar <faisal.akhtar@tecsys.com>
2022-07-31 15:36:01 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-input v-model="input" placeholder="Please input" /> 
+  <el-button @click="alertFunction">Default</el-button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import apiClient from './core/apiclient.js'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup(){
+    const input = ref('')
+    function alertFunction() {
+      console.log(input.value);
+    }
+    return{input, alertFunction}  
   }
 }
 </script>
@@ -19,8 +28,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
